ef665a3a61fc106518e80a4b019805d5a9350b10
Andres Moschini <andresmoschini@gmail.com>
2015-12-01 08:40:39 -0300

    Remove ignored files (again)

--- typings/app.d.ts
+++
@@ -1,6 +0,0 @@
-//{
-/// <reference path="../src/greeter.ts" />
-/// <reference path="../src/scripts.ts" />
-/// <reference path="../src/_defaults.d.ts" />
-/// <reference path="../src/_tsd.d.ts" />
-//}
