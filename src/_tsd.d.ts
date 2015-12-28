0a8b793cbd0d611acf9d2b86da135223a70ded3c
Lucas Marquez <lucasasecas@gmail.com>
2015-12-28 16:48:52 -0300

    Remove tds generated file and replace tds instal by tds update

--- src/_tsd.d.ts
+++ src/_tsd.d.ts
@@ -1,3 +1,2 @@
 /// <reference path="../typings/knockout/knockout.d.ts" />
-/// <reference path="../node_modules/tsd/node_modules/definition-header/dist/index.d.ts" />
 /// <reference path="../typings/socket.io-client/socket.io-client.d.ts" />
